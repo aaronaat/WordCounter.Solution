49201edcc72ee70f1a3129f67595d1b8df16752b
Aaron Taylor <aarontaylor@gmail.com>
2019-02-15 22:58:41 -0800

    update model and test to include word string in constructor

--- WordCounter.Tests/ModelTests/CountTest.cs
+++ WordCounter.Tests/ModelTests/CountTest.cs
@@ -7,13 +7,17 @@ namespace WordCounter.Tests
   public class RepeatCounterTest
   {
     [TestMethod]
-    public void SentenceConstructor_CreateSentence_Sentence()
+    public void Constructor_CreateSetWordSentence_WordSentence()
     {
+      string wrd = "word";
       string sent = "This is a sentence";
-      RepeatCounter newSentence = new RepeatCounter(sent);
+      RepeatCounter newSentence = new RepeatCounter(wrd, sent);
       string sent2 = newSentence.GetSentence();
+      string wrd2 = newSentence.GetWord();
       Assert.AreEqual(sent, sent2);
+      Assert.AreEqual(wrd, wrd2);
     }
+
   }
 
 
