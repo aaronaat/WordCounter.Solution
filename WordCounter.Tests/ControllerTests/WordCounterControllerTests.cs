dc1722aa02c0dd9d14d079b65fd16827c531d74d
aaron taylor <aarontaylor@gmail.com>
2019-02-22 11:59:59 -0800

    add tests

--- WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -9,6 +9,37 @@ namespace WordCounter.Tests
     [TestClass]
     public class WordCounterControllerTest
     {
+      [TestMethod]
+      public void Index_ReturnsCorrectView_True()
+      {
+        WordCounterController controller = new WordCounterController();
+        ActionResult indexView = controller.Index();
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
+      [TestMethod]
+      public void CreateCount_ReturnsCorrectView_True()
+      {
+        WordCounterController controller = new WordCounterController();
+        ActionResult indexView = controller.CreateCount();
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
+      [TestMethod]
+      public void Create_ReturnsCorrectView_True()
+      {
+        WordCounterController controller = new WordCounterController();
+        ActionResult indexView = controller.Create("test", "test");
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
+
+
+      [TestMethod]
+      public void Create_HasCorrectModelType_RepeatCounter()
+      {
+        ViewResult indexView = new WordCounterController().Create("index", "index") as ViewResult;
+        var result = indexView.ViewData.Model;
+        Assert.IsInstanceOfType(result, typeof(RepeatCounter));
+      }
+
 
     }
 }
